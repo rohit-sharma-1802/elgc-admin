8f876cc0f53689735f35eca463ba62602b3e08c4
officialrohitsharma <rohitsharmatech9@gmail.com>
2023-06-07 21:40:06 +0530

    project uploaded

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -1,16 +1,16 @@
 var jwt = require("jsonwebtoken");
 module.exports = {
-  checkToken: (req, res, next) => {
-    let token = req.headers["authorization"];
-    console.log(token);
-    token = token.slice(7);
-    jwt.verify(token, process.env.WEBTOKEN_KEY, function (err, decoded) {
-      if (err)
-        return res
-          .status(500)
-          .send({ auth: false, message: "Failed to authenticate token." });
+    checkToken: (req, res, next) => {
+        let token = req.headers["authorization"];
+        console.log(token);
+        token = token.slice(7);
+        jwt.verify(token, process.env.WEBTOKEN_KEY, function(err, decoded) {
+            if (err)
+                return res
+                    .status(500)
+                    .send({ auth: false, message: "Failed to authenticate token." });
 
-      next();
-    });
-  },
+            next();
+        });
+    },
 };
