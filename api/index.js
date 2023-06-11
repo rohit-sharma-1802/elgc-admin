042cfbe115e91d42c2013461e0574265e7976e50
rohit-sharma-1802 <59288117+rohit-sharma-1802@users.noreply.github.com>
2023-06-11 18:39:44 +0530

    Create index.js

---
+++ api/index.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+const adminRouter = require("./Admin/Admin.route");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(fileUpload());
+
+app.use("/api/admin", adminRouter);
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server is Running for Admin");
+});
